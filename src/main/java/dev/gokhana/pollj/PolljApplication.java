98644234a20661f90d7014d1b586eee6e6e0595e
Gökhan Ayrancıoğlu <gkhan.a.06@gmail.com>
2021-12-06 00:14:54 +0300

    github login integration

---
+++ src/main/java/dev/gokhana/pollj/PolljApplication.java
@@ -0,0 +1,13 @@
+package dev.gokhana.pollj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolljApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolljApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gokhana/pollj/PolljApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gokhana.pollj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolljApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
